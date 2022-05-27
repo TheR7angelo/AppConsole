9a8f1c2906a6753fd6d0d4e3c9a650b29f3adaeb
ZP6177 <raphael.antunes@hotmail.fr>
2022-05-27 15:55:37 +0200

    init

---
+++ AppConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AppConsole
+{
+    /*
+     *  1. Le joueur est dans une foret. Il peut aller à l'est ou à l'ouest.
+     *      1.1 Si le choix est est zlors il tombe et à perdu
+     *      1.2 Si ouest alors le jeux continue
+     *  2. Le joueur est devant le chateau. Il peut entrer par la porte ou par la fenetre.
+     *      2.1 Si porte = Un garde l'arrete Game Over
+     *      2.2 Si fenetre alors le jeu continue
+     *  3. Le joueur est dans le couloir du chateau. Il peut ouvrir la porte de droite ou bien de gauche.
+     *      3.1 Si porte gauche = Il tombe sur des des gardes Game Over.
+     *      3.2 Si droite = Il trouve la princesse WINNE
+     *
+     *  Suite :
+     *      - Ajouter un inventaire, clef, objet.
+     *      - Plus de route.
+     *      - Système de score, vie, magie.
+     */
+    
+    
+    
+    static void Main(String[] args)
+    {
+        Console.WriteLine("test");
+    }
+    
+}
